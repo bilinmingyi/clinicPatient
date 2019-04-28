fe2df0826a275c1798cc92b4bb14c8bec3e6987f
wzh <wzh@mybilin.cn>
2019-04-28 11:59:32 +0800

    订单详情

--- src/components/page/personal/mallOrderDetail.vue
+++ src/components/page/personal/mallOrderDetail.vue
@@ -7,8 +7,36 @@
           <span class="flexOne ml-16px">订单信息</span>
           <span class="label-red">待支付</span>
         </Small-title>
-        <div>
-          相关信息
+        <div class="order-infor">
+          <div class="flex mb-8px">
+            <div class="label-text">下单时间：</div>
+            <div class="input-text">2018-12-23 22:26</div>
+          </div>
+          <div class="flex mb-8px">
+            <div class="label-text">王大锤：</div>
+            <div class="input-text">82082088520</div>
+          </div>
+          <div class="flex mb-8px">
+            <div class="label-text">备注：</div>
+            <div class="input-text">早上起床头疼发热</div>
+          </div>
+          <div class="flex mb-8px">
+            <div class="label-text"> 收件人：</div>
+            <div class="input-text">王尼玛</div>
+          </div>
+
+          <div class="flex mb-8px">
+            <div class="label-text"> 电话：</div>
+            <div class="input-text">15888654678</div>
+          </div>
+          <div class="flex">
+            <div class="label-text">收货地址：</div>
+            <div class="input-text">广州市天河区珠江新城花城汇XXX街道幸 幸福小区小区6栋601</div>
+          </div>
+          <div class="flex mb-8px">
+            <div class="label-text">发货方式：</div>
+            <div class="input-text">自提</div>
+          </div>
         </div>
       </div>
       <div class="content-back">
@@ -143,6 +171,24 @@ export default {
     color: $depthTextColor;
     line-height: 45px;
     font-size: 32px;
+  }
+
+  .order-infor {
+    padding: 20px 30px;
+    line-height: 42px;
+
+    .label-text {
+      color: $lightTextColor;
+      font-size: 30px;
+      width: 150px;
+      text-align: right;
+    }
+
+    .input-text {
+      color: $depthTextColor;
+      font-size: 30px;
+      flex: 1;
+    }
   }
 
   .label-red {
