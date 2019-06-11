a0f353e856d7d84ed5b68711549270571caf81b7
wuzd <429023964@qq.com>
2019-06-11 17:11:20 +0800

    预约订单详情修改

--- src/components/page/personal/appointOrderDetail.vue
+++ src/components/page/personal/appointOrderDetail.vue
@@ -14,12 +14,12 @@
           </div>
           <div class="mb-8px">
             <span class="label-three">预约时间：</span>
-            <span class="label-two">{{orderInfo.appointDate|dateFormat('yyyy-MM-dd   W   ')}}{{orderInfo.startTime}}-{{orderInfo.endTime}}</span>
+            <span class="label-two">{{orderInfo.appoint_date|dateFormat('yyyy-MM-dd   W   ')}}{{orderInfo.start_time}}-{{orderInfo.end_time}}</span>
           </div>
           <div class="mb-8px">
             <span class="label-three">预约医生：</span>
             <span
-              class="label-two">{{orderInfo.doctorName}}</span>
+              class="label-two">{{orderInfo.doctor_name}}</span>
           </div>
           <div>
             <span class="label-three">挂号费用：</span>
@@ -34,22 +34,22 @@
         <div class="patient-infor">
           <div class="line-item">
             <label class="label-span mr-32px">手机号码</label>
-            <span class="label-span">{{orderInfo.patientMobile}}</span>
+            <span class="label-span">{{orderInfo.patient_mobile}}</span>
           </div>
           <hr class="line-hr">
           <div class="line-item">
             <label class="label-span mr-96px">姓名</label>
-            <span class="label-span">{{orderInfo.patientName}}</span>
+            <span class="label-span">{{orderInfo.patient_name}}</span>
           </div>
           <hr class="line-hr">
           <div class="line-item">
             <label class="label-span mr-96px">性别</label>
-            <span class="label-span">{{orderInfo.patientSex|sexFormat}}</span>
+            <span class="label-span">{{orderInfo.patient_sex|sexFormat}}</span>
           </div>
           <hr class="line-hr">
           <div class="line-item">
             <label class="label-span mr-96px">年龄</label>
-            <span class="label-span">{{orderInfo.patientAge}}</span>
+            <span class="label-span">{{orderInfo.patient_age}}</span>
           </div>
         </div>
       </div>
@@ -101,9 +101,9 @@ export default {
         order_seqno: this.orderSeqno
       }).then(res => {
         if (res.code === 1000) {
-          this.orderInfo = res.data.order_info
-          let time = this.orderInfo.appointDate
-          this.orderInfo.appointDate = new Date(time.substr(0, 4) + '-' + time.substr(4, 2) + '-' + time.substr(6, 2))
+          this.orderInfo = res.data
+          // let time = this.orderInfo.appoint_date
+          // this.orderInfo.appoint_date = new Date(time.substr(0, 4) + '-' + time.substr(4, 2) + '-' + time.substr(6, 2))
         } else {
           this.$Message.infor(res.msg)
         }
